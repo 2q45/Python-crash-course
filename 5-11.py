5b094e0f14b3a77f712125748b300e5e429649ff
agrawi1 <agrawi1@kgv.hk>
2021-07-19 18:39:28 +0800

    5-11

--- 5-11.py
+++ 5-11.py
@@ -1,11 +1,11 @@
 print("ordinal numbers")
 ordinal = [1,2,3,4,5,6,7,8,9]
 for number in ordinal:
-        if number == "1":
-            print("{number}st")
-        if number == "2":
-            print("{number}nd")
-        if number == "3":
-            print("{number}rd")
+        if number == 1:
+            print(f"{number}st")
+        elif number == 2:
+            print(f"{number}nd")
+        elif number == 3:
+            print(f"{number}rd")
         else:
-            print("{number}th")
+            print(f"{number}th")
