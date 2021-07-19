1228a9b70839ef9a9ff72dabc7c7d67c101d28ec
agrawi1 <agrawi1@kgv.hk>
2021-07-19 19:03:59 +0800

    lakshjDVG

--- 4-1.py
+++ 4-1.py
@@ -2,3 +2,5 @@ PIZZAS = ["Four Cheese", "Margherita", "Mushroom"]
 for Pizza in PIZZAS:
     print(f"I like {Pizza} pizza\n")
 print("I really Like pizza")
+
+
---
+++ squares.py
@@ -0,0 +1,4 @@
+squares = [1,2,3,4,5,6,7,8,9,10]
+for number in squares:
+    number = number**2
+    print(number)
